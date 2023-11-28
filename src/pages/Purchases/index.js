e3d08e26a56d9fd5656cd29da1905699ca4bfa78
agette_meli <alan.gette@mercadolibre.com>
2023-11-28 10:13:08 -0300

    fix purchases

--- src/pages/Purchases/index.js
+++ src/pages/Purchases/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./index.scss";
 
 import Title from '../../components/Title';
+import PurchasesSvg from '../../assets/purchases.svg';
 
 export default function Purchases({reference}) {
+  const [purchases, setPurchases] = useState();
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <section className="purchases" ref={reference}>
       <Title text="Purchases" />
-      <section className="container">
+      {purchases && (
+        <section className="purchases-list">
+
+        </section>
+      )}
+      <section className="purchases-empty-state">
+        <div className="svg-description">
+          <img className="account-svg" src={PurchasesSvg}></img>
+        </div>
+        <div className="empty-description">
+          <span className="paragraph">Here you can see your purchases.</span>
+          <span className="paragraph"><Link to='/login'>Sign in</Link> to view your account.</span>
+        </div>
       </section>
     </section>
   );
