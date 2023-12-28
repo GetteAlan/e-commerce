09a7ea656a42d714d3e49e81d3bc35b5d91172ee
agette_meli <alan.gette@mercadolibre.com>
2023-12-27 23:58:47 -0300

    add sign up

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./index.scss";
+
+import Title from '../../components/Title';
+import Button from '../../components/Button';
+import Loading from '../../components/Loading';
+import { useAuth } from "../../providers/authProvider";
+import { useCart } from "../../providers/cartProvider";
+
+export default function SignUp({reference}) {
+  const { setToken, setAccount } = useAuth();
+  const { fetchCurrentCart } = useCart();
+  const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [errorLogin, setErrorLogin] = useState(false);
+  const navigate = useNavigate();
+
+  const onSubmitHandling = (event) => {
+    event.preventDefault();
+
+    if (!username || !password) {
+      return;
+    }
+
+    setIsLoading(true);
+    setErrorLogin(false);
+
+    fetch('https://e-commerce.gettealan.com/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      })
+    })
+    .then(async (response) => {
+      const result = await response.json();
+      const token = result?.token;
+      const account = result?.account;
+
+      setAccount(account);
+      setToken(token);
+
+      if (token && account) {
+        await fetchCurrentCart(account.id);
+        navigate('/');
+
+        return;
+      }
+
+      throw new Error('Error login');
+    }).catch((error) => {
+      console.log('Error', error);
+      setAccount();
+      setToken();
+      setErrorLogin(true);
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    const currentToken = sessionStorage.getItem('token');
+
+    if (currentToken) {
+      //navigate('/');
+    }
+
+  }, []);
+
+  return (
+    <section className="login" ref={reference}>
+      <Title text="Sign Up"/>
+      <section className="login-container">
+        <form className="login-form" onSubmit={onSubmitHandling}>
+          <div className="inputs-container">
+            <input placeholder="First and last name" className="input" name="name" type="text" onChange={e => setUsername(e.target.value)} disabled={isLoading}/>
+            <input placeholder="Email" className="input" name="email" type="text" onChange={e => setUsername(e.target.value)} disabled={isLoading}/>
+            <input placeholder="Password" className="input" name="password" type="password" onChange={e => setPassword(e.target.value)} disabled={isLoading}/>
+            <input placeholder="Re-enter password" className="input" name="password" type="password" onChange={e => setPassword(e.target.value)} disabled={isLoading}/>
+
+          </div>
+          <div className="button-container">
+            { isLoading ? (
+              <div className="loading-wrapper">
+                <Loading />
+              </div>              
+            ) : (
+              <Button text="Sing up"></Button>
+            )}
+          </div>
+        </form>
+      </section>
+      { errorLogin && (
+        <section className="messages-wrapper">
+          <span className="message error">Error trying to login.</span>
+        </section>
+      )}
+    </section>
+  );
+}
